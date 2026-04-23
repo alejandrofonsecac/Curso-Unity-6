c451a0ef43f0f8a62b7ac7ac575b538e45ea6bc7
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2026-04-23 19:26:57 -0300

    String e concactenação

---
+++ learning-csharp/03-Strings/Program.cs
@@ -0,0 +1,14 @@
+﻿string hello = "Olá, ";
+string nome = "Astolfo";
+int idade = 20;
+
+Console.Write(hello);
+Console.Write(nome);
+
+Console.WriteLine("--------");
+
+Console.WriteLine(hello + nome + ", Sua idade é " + idade);
+
+Console.WriteLine("====");
+
+Console.WriteLine($"{hello}, {nome}, Sua idade {idade}");
